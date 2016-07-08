5ba063f0d0cfa1fa961a0fec3b69a214cedc6189
Sacha Bron <sacha.bron@copperflake.com>
2016-07-08 17:30:30 +0200

    Basic Recording app

---
+++ Hello-Stream-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Hello-Stream-Bridging-Header.h
+//  Hello-Stream
+//
+//  Created by Sacha Bron on 07/07/16.
+//  Copyright © 2016 Sacha Bron. All rights reserved.
+//
+
+#ifndef Hello_Stream_Bridging_Header_h
+#define Hello_Stream_Bridging_Header_h
+
+#import "KFRecorder.h"
+
+#endif /* Hello_Stream_Bridging_Header_h */
--- Hello-Stream/ViewController.swift
+++ Hello-Stream/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+	// @IBOutlet weak var viewCamera: UIView!
+	@IBOutlet weak var recordBtn: UIButton!
+
+	let recorder = KFRecorder(name: "test")
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+
+		let parentLayer: CALayer = self.view.layer
+
+		var previewLayer = AVCaptureVideoPreviewLayer()
+		previewLayer = recorder.previewLayer
+		parentLayer.addSublayer(previewLayer)
+		previewLayer.frame = parentLayer.frame
+
+		// previewLayer.setVideoGravity(AVLayerVideoGravityResizeAspectFill)
+		// previewLayer.connection.videoOrientation(AVCaptureVideoOrientationLandscapeRight);
+
+		// let layerRect: CGRect = viewCamera!.bounds;
+
+		// previewLayer.setBounds(layerRect);
+		// previewLayer.setPosition(CGPointMake(CGRectGetMidX(layerRect), CGRectGetMidY(layerRect)));
+
+		// viewCamera.layer.addSublayer(previewLayer);
+
+		recorder.session.startRunning()
+		recorder.startRecording()
+		
+		// [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(newAssetGroupCreated:) name:NotifNewAssetGroupCreated object:nil];
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +46,8 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	@IBAction func recordTapped(sender: UIButton) {
+		print("recordButtonTapped")
+	}
 }
 
