63deb0963ec90a20cee9f93c2adcd4254c186e90
Sacha Bron <sacha.bron@copperflake.com>
2016-07-22 18:02:28 +0200

    Add class LiveStream

---
+++ Hello-Stream/LiveStream.swift
@@ -0,0 +1,94 @@
+//
+//  LiveStream.swift
+//  Hello-Stream
+//
+//  Created by Sacha Bron on 22/07/16.
+//  Copyright © 2016 Sacha Bron. All rights reserved.
+//
+
+import Foundation
+
+class LiveStream {
+	let maxBitrate = 3 * 1024 * 1024 // 3 Mbps
+	let minBitrate = 1 * 1024 // 1 Kbps
+	let uploadDurationRatio = 0.7
+	
+	let recorder = KFRecorder(name: "test")
+	let uploader = Uploader(endpoint: NSURL(string: "http://192.168.1.121:3000")!)
+
+	let filesURL = NSURL(fileURLWithPath: Utilities.applicationSupportDirectory())
+
+	init () {
+		recorder.session.startRunning()
+		
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(newAssetGroupCreated), name: NotifNewAssetGroupCreated, object: nil)
+	}
+	
+	func record() {
+		recorder.startRecording()
+	}
+	
+	func stop() {
+		recorder.stopRecording()
+	}
+	
+	func isRecording() -> Bool {
+		return recorder.isRecording
+	}
+	
+	@objc func newAssetGroupCreated(notification: NSNotification) {
+		print("-- New Asset Group - %@", notification.object);
+		
+		let info = notification.object as! AssetGroup
+		let segmentDuration = info.duration
+		let filePath: NSURL = filesURL.URLByAppendingPathComponent(info.fileName)
+		let manifestPath = NSURL(fileURLWithPath: info.manifestName)
+		
+		uploadSegment(filePath, segmentDuration: segmentDuration)
+		uploadPlaylist(manifestPath)
+	}
+	
+	func uploadSegment(filePath: NSURL, segmentDuration: Double) {
+		print("-- File \(filePath.path!) (\(segmentDuration)s)")
+		let startTime = NSDate()
+		
+		uploader.addFileToQueue(filePath) { (obj, success) in
+			let endTime = NSDate()
+			let uploadDuration: Double = endTime.timeIntervalSinceDate(startTime)
+			
+			// Compute the new bitrate to optimize upload time and
+			var newBitrate = Int(Double(self.recorder.h264Encoder.bitrate) * segmentDuration * self.uploadDurationRatio / uploadDuration)
+			newBitrate = min(newBitrate, self.maxBitrate)
+			newBitrate = max(newBitrate, self.minBitrate)
+			print("New Bitrate: " + String(newBitrate))
+			self.recorder.h264Encoder.bitrate = Int32(newBitrate)
+			
+			if (success ?? false) {
+				print("Upload successful")
+			} else {
+				print("Error while sending file")
+				print(obj)
+			}
+			
+			print("Upload time: \(uploadDuration)s")
+		}
+	}
+	
+	func uploadPlaylist(manifestPath: NSURL) {
+		print("-- Manifest \(manifestPath.path!)")
+		
+		if (!NSFileManager.defaultManager().fileExistsAtPath(manifestPath.path!)) {
+			print ("Manifest doesn't exist")
+			return
+		}
+		
+		uploader.addFileToQueue(manifestPath) { (obj, success) in
+			if (success ?? false) {
+				print("Upload manifest successful")
+			} else {
+				print("Error while sending manifest")
+				print(obj)
+			}
+		}
+	}
+}
--- Hello-Stream/ViewController.swift
+++ Hello-Stream/ViewController.swift
@@ -9,17 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-	let maxBitrate = 3 * 1024 * 1024 // 3 Mbps
-	let minBitrate = 1 * 1024 // 1 Kbps
-	let uploadDurationRatio = 0.7
-
 	@IBOutlet weak var recordBtn: UIButton!
-
-	let recorder = KFRecorder(name: "test")
-	let uploader = Uploader(endpoint: NSURL(string: "http://192.168.1.121:3000")!)
-
-	let filesURL = NSURL(fileURLWithPath: Utilities.applicationSupportDirectory())
-
+	
+	let streamer = LiveStream()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -27,10 +20,10 @@ class ViewController: UIViewController {
 		let parentLayer: CALayer = self.view.layer
 
 		var previewLayer = AVCaptureVideoPreviewLayer()
-		previewLayer = recorder.previewLayer
+		previewLayer = streamer.recorder.previewLayer
 		parentLayer.addSublayer(previewLayer)
 		previewLayer.frame = parentLayer.frame
-
+		
 		// previewLayer.setVideoGravity(AVLayerVideoGravityResizeAspectFill)
 		// previewLayer.connection.videoOrientation(AVCaptureVideoOrientationLandscapeRight);
 
@@ -41,71 +34,12 @@ class ViewController: UIViewController {
 
 		// viewCamera.layer.addSublayer(previewLayer);
 
-		recorder.session.startRunning()
-		recorder.startRecording()
-
-		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(newAssetGroupCreated), name: NotifNewAssetGroupCreated, object: nil)
+		streamer.record()
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
-	}
-
-	func newAssetGroupCreated(notification: NSNotification) {
-		print("-- New Asset Group - %@", notification.object);
-
-		let info = notification.object as! AssetGroup
-		let segmentDuration = info.duration
-		let filePath: NSURL = filesURL.URLByAppendingPathComponent(info.fileName)
-		let manifestPath = NSURL(fileURLWithPath: info.manifestName)
-		
-		uploadSegment(filePath, segmentDuration: segmentDuration)
-		uploadPlaylist(manifestPath)
-	}
-
-	func uploadSegment(filePath: NSURL, segmentDuration: Double) {
-		print("-- File \(filePath.path!) (\(segmentDuration)s)")
-		let startTime = NSDate()
-
-		uploader.addFileToQueue(filePath) { (obj, success) in
-			let endTime = NSDate()
-			let uploadDuration: Double = endTime.timeIntervalSinceDate(startTime)
-
-			// Compute the new bitrate to optimize upload time and
-			var newBitrate = Int(Double(self.recorder.h264Encoder.bitrate) * segmentDuration * self.uploadDurationRatio / uploadDuration)
-			newBitrate = min(newBitrate, self.maxBitrate)
-			newBitrate = max(newBitrate, self.minBitrate)
-			print("New Bitrate: " + String(newBitrate))
-			self.recorder.h264Encoder.bitrate = Int32(newBitrate)
-
-			if (success ?? false) {
-				print("Upload successful")
-			} else {
-				print("Error while sending file")
-				print(obj)
-			}
-
-			print("Upload time: \(uploadDuration)s")
-		}
-	}
-
-	func uploadPlaylist(manifestPath: NSURL) {
-		print("-- Manifest \(manifestPath.path!)")
-
-		if (!NSFileManager.defaultManager().fileExistsAtPath(manifestPath.path!)) {
-			print ("Manifest doesn't exist")
-			return
-		}
-
-		uploader.addFileToQueue(manifestPath) { (obj, success) in
-			if (success ?? false) {
-				print("Upload manifest successful")
-			} else {
-				print("Error while sending manifest")
-				print(obj)
-			}
-		}
 	}
 
 	@IBAction func recordTapped(sender: UIButton) {
